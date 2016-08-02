2a746c4c9716774f9c46b9018298a20a76af4214
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-08-02 18:40:48 +0200

    Dropped dependency to appcompat-v7 for now, disabled InvalidPackage lint error for now.

--- app/src/main/java/com/github/clboettcher/bonappetit/app/MainActivity.java
+++ app/src/main/java/com/github/clboettcher/bonappetit/app/MainActivity.java
@@ -1,18 +1,17 @@
 package com.github.clboettcher.bonappetit.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
