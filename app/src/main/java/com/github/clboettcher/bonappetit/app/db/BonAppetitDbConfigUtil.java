7436f320577bc202a69ecf935116fe14b75fbfd4
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-10-01 18:31:37 +0200

    Added db setup classes and run config to generate the ormlite conf file.

---
+++ app/src/main/java/com/github/clboettcher/bonappetit/app/db/BonAppetitDbConfigUtil.java
@@ -0,0 +1,30 @@
+package com.github.clboettcher.bonappetit.app.db;
+
+import com.github.clboettcher.bonappetit.app.staff.StaffMemberEntity;
+import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class BonAppetitDbConfigUtil extends OrmLiteConfigUtil {
+
+    /**
+     * All classes that are mapped to a db table.
+     */
+    public static final Class<?>[] DATABASE_CLASSES = new Class[]{
+            // Staff
+            StaffMemberEntity.class
+    };
+
+    /**
+     * Creates the config file that makes ORM lite fast.
+     * <p/>
+     * For this to work you have to:
+     * <p/>
+     * - Run with work dir \app\src\main
+     * - Create dir res/raw
+     */
+    public static void main(String[] args) throws SQLException, IOException {
+        writeConfigFile("ormlite_config.txt", DATABASE_CLASSES);
+    }
+}
