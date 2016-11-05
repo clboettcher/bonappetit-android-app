df1c299cde3eacd783dd62c4bfbf68dda0c0789f
Claudius Böttcher <c.boettcher@muc.qaware.local>
2016-11-05 19:40:10 +0100

    Removed already resolved TODO.

--- app/src/main/java/com/github/clboettcher/bonappetit/app/ui/menu/MenuFragment.java
+++ app/src/main/java/com/github/clboettcher/bonappetit/app/ui/menu/MenuFragment.java
@@ -34,7 +34,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-// TODO: add on click listener for update failed retry button
 public class MenuFragment extends TakeOrdersFragment {
 
     private static final String TAG = MenuFragment.class.getName();
